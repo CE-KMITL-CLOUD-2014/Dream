ee68eaa34702f9f8b9bafeff1ea373fc3ad8ba6c
CEmocca <champ_alone_always@hotmail.com>
2014-10-19 07:50:56 +0700

    add dream-money Front end

---
+++ dreamWebsite/WebContent/angular_header.js
@@ -0,0 +1,13 @@
+
+
+<script type="text/ng-template" id="angular_header.html">
+
+
+	  	<md-toolbar class="md-theme-dark">
+		    <div class="md-toolbar-tools">
+		      	<span class="md-flex">Toolbar: dark-theme</span>
+		    </div>
+	  	</md-toolbar>
+
+
+</script>
---
+++ dreamWebsite/WebContent/control/angular_head.js
@@ -0,0 +1,13 @@
+var app = angular.module('debtCal', [])
+
+app.controller('headCtrl', ['$scope', function($scope) {
+
+}])
+
+app.controller('inputCtrl', ['$scope', function($scope) {
+	$scope.someLabel = "ABCDEF";
+}])
+
+app.controller('footCtrl', ['$scope', function($scope) {
+
+}])
